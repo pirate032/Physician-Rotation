9682c9f827b9abfa4960eb1df28a2fc27e1f446e
pirate032 <pirate032@gmail.com>
2024-09-19 12:08:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const searchBtn = document.getElementById("search-btn")
 //console.log(diagnosisEl)
 //create an array of physician/last consult pairs
 let physicians = [
-    ["Mohammad Eslami",False],
-    ["Ali AbuRahma",False],
-    ["Matthew Beasley",False],
-    ["Andrew Lee",False],
-    ["Catherine Go",False],
-    ["Zach AbuRahma",False]
+    ["Mohammad Eslami","0"],
+    ["Ali AbuRahma","0"],
+    ["Matthew Beasley","0"],
+    ["Andrew Lee","0"],
+    ["Catherine Go","0"],
+    ["Zach AbuRahma","0"]
 ]
-
+console.log(physicians)
 
 
 searchBtn.addEventListener("click", function() {
