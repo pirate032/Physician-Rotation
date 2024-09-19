7918c349fb7aa45de5c79d499c1a0944aaa8ce6f
pirate032 <pirate032@gmail.com>
2024-09-19 11:50:07 -0400

    Update index.js
    scrap the DB for now

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
 
-import { getDatabase,
-    ref,
-    push,
-    onValue,
-    remove } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
+// import { getDatabase,
+//     ref,
+//     push,
+//     onValue,
+//     remove } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
 
-const firebaseConfig = {
-   databaseURL: "https://physicianpicker-default-rtdb.firebaseio.com/"
-}
+// const firebaseConfig = {
+//    databaseURL: "https://physicianpicker-default-rtdb.firebaseio.com/"
+// }
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-const referenceInDB = ref(database, "physicians")
+// const app = initializeApp(firebaseConfig)
+// const database = getDatabase(app)
+// const referenceInDB = ref(database, "physicians")
 
 const diagnosisEl = document.getElementById("diagnoses-el").value
 const searchBtn = document.getElementById("search-btn")
 console.log(diagnosisEl)
+//create an array of physician/last consult pairs
+let physicians = [
+    ["Mohammad Eslami",False],
+    ["Ali AbuRahma",False],
+    ["Matthew Beasley",False],
+    ["Andrew Lee",False],
+    ["Catherine Go",False],
+    ["Zach AbuRahma",False]
+]
 
-function setPhysicians () {
-    console.log("creating physician DB")
-    push(referenceInDB, ["Mohammad Eslami",False])
-    push(referenceInDB, ["Ali AbuRahma",False])
-    push(referenceInDB, ["Matthew Beasley",False])
-    push(referenceInDB, ["Andrew Lee",False])
-    push(referenceInDB, ["Catherine Go",False])
-    push(referenceInDB, ["Zach AbuRahma",False])
-}
+// function setPhysicians () {
+//     console.log("creating physician DB")
+//     push(referenceInDB, ["Mohammad Eslami",False])
+//     push(referenceInDB, ["Ali AbuRahma",False])
+//     push(referenceInDB, ["Matthew Beasley",False])
+//     push(referenceInDB, ["Andrew Lee",False])
+//     push(referenceInDB, ["Catherine Go",False])
+//     push(referenceInDB, ["Zach AbuRahma",False])
+// }
 
-// onValue (referenceInDB, function(snapshot) {
+//onValue (referenceInDB, function(snapshot) {
 //     const snapshotDoesExist = snapshot.exists()
 //     if (snapshotDoesExist) {
 //         const snapshotValues = snapshot.val()
@@ -38,13 +47,14 @@ function setPhysicians () {
 //     } else {
 //         console.log("no snapshot") 
 //     }
-// })
+//    console.log(snapshot)
+//})
 
 searchBtn.addEventListener("click", function() {
     //push(referenceInDB, searchEl.value)
     //inputEl.value = "" 
-    console.log("search clicked")
+    console.log("search clicked" + diagnosisEl.value)
 })
 
-setPhysicians()
+//setPhysicians()
 
