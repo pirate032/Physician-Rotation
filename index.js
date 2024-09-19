2d45af9aeb7dfb5a60d74c97e006acf80e238eca
pirate032 <pirate032@gmail.com>
2024-09-19 11:27:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js"
 
 import { getDatabase,
     ref,
@@ -28,24 +28,17 @@ function setPhysicians () {
     push(referenceInDB, ["Zach AbuRahma",False])
 }
 
-onValue (referenceInDB, function(snapshot) {
-    const snapshotDoesExist = snapshot.exists()
-    if (snapshotDoesExist) {
-        //const snapshotValues = snapshot.val()
-        //const leads = Object.values(snapshotValues)
-        //render(leads)
-        console.log("snapshot exists")
-    } else {
-        console.log("no snapshot")
-        //console.log("creating physician DB")
-        //push(referenceInDB, ["Mohammad Eslami",False])
-        //push(referenceInDB, ["Ali AbuRahma",False])
-        //push(referenceInDB, ["Matthew Beasley",False])
-        //push(referenceInDB, ["Andrew Lee",False])
-        //push(referenceInDB, ["Catherine Go",False])
-        //push(referenceInDB, ["Zach AbuRahma",False])
-    }
-})
+// onValue (referenceInDB, function(snapshot) {
+//     const snapshotDoesExist = snapshot.exists()
+//     if (snapshotDoesExist) {
+//         const snapshotValues = snapshot.val()
+//         /onst leads = Object.values(snapshotValues)
+//         render(leads)
+//         console.log("snapshot exists")
+//     } else {
+//         console.log("no snapshot") 
+//     }
+// })
 
 searchBtn.addEventListener("click", function() {
     //push(referenceInDB, searchEl.value)
@@ -54,3 +47,4 @@ searchBtn.addEventListener("click", function() {
 })
 
 setPhysicians()
+
