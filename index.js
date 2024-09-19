bafe562a78a068494bc4727105aafa0b3b73d661
pirate032 <pirate032@gmail.com>
2024-09-19 13:01:49 -0400

    cleaning up

--- index.js
+++ index.js
@@ -13,9 +13,60 @@ let physicians = [
     ["Zach AbuRahma",0]
 ]
 
+//create an array of diagnoses
+// let diagnoses = [
+//         "None",
+//         "AAA",
+//         "Bow Hunter",
+//         "Carotid Artery Stenosis",
+//         "Carotid Body Tumor",
+//         "Celiac Artery Compression Syndrome",
+//         "Claudication",
+//         "Dissections - Chronic",
+//         "DVT",
+//         "ESRD - PCS - AVF - AVGS",
+//         "Hemodialysis Access",
+//         "Iliac Aneurysm",
+//         "Lymphedema",
+//         "May-Thurner",
+//         "Mesenteric - Celiac Stenosis",
+//         "Nutcracker Syndrome",
+//         "Peripheral Artery Occlusive Disease",
+//         "Popliteal Aneurysm",
+//         "Pseudoaneurysm",
+//         "Raynauds",
+//         "Renal Artery Stenosis",
+//         "Renal FMD",
+//         "Splenic Aneurysm",
+//         "Subclavian Steal",
+//         "Subclavian Artery Stenosis",
+//         "Temporal Arteritis",
+//         "Thoracic Aorta Aneurysm",
+//         "Vericose Veins",
+//         "Vasculitis",
+//         "Venous Insufficiency",
+//         "Vertebral Artery Stenosis",
+//         "Williams Syndrome",   
+// ]
+
 console.log(physicians)
 console.log(diagnosesEl)
 
+// function render(diagnoses) {
+//     let listItems = ""
+//     for (let i = 0; i < diagnoses.length; i++) {
+//         listItems += `
+//             <li>
+//                 <a target='_blank' href='${diagnoses[i]}'>
+//                     ${diagnoses[i]}
+//                 </a>
+//             </li>
+//         `
+//     }
+//     diagnosesEl.innerHTML = listItems
+// }
+
 searchBtn.addEventListener("click", function() {
-    console.log(diagnosesEl.nodeValue)
+    console.log(diagnosesEl).value
 })
+
