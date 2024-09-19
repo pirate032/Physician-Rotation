8fa80650346d27d136d191a32b8c732af6186127
pirate032 <pirate032@gmail.com>
2024-09-19 11:03:55 -0400

    create physician DB

--- index.js
+++ index.js
@@ -18,12 +18,32 @@ const diagnosisEl = document.getElementById("diagnoses-el").value
 const searchBtn = document.getElementById("search-btn")
 console.log(diagnosisEl)
 
-onValue(referenceInDB, function(snapshot) {
+function setPhysicians () {
+    console.log("creating physician DB")
+    push(referenceInDB, ["Mohammad Eslami",False])
+    push(referenceInDB, ["Ali AbuRahma",False])
+    push(referenceInDB, ["Matthew Beasley",False])
+    push(referenceInDB, ["Andrew Lee",False])
+    push(referenceInDB, ["Catherine Go",False])
+    push(referenceInDB, ["Zach AbuRahma",False])
+}
+
+onValue (referenceInDB, function(snapshot) {
     const snapshotDoesExist = snapshot.exists()
     if (snapshotDoesExist) {
-        const snapshotValues = snapshot.val()
-        const leads = Object.values(snapshotValues)
-        render(leads)
+        //const snapshotValues = snapshot.val()
+        //const leads = Object.values(snapshotValues)
+        //render(leads)
+        console.log("snapshot exists")
+    } else {
+        console.log("no snapshot")
+        //console.log("creating physician DB")
+        //push(referenceInDB, ["Mohammad Eslami",False])
+        //push(referenceInDB, ["Ali AbuRahma",False])
+        //push(referenceInDB, ["Matthew Beasley",False])
+        //push(referenceInDB, ["Andrew Lee",False])
+        //push(referenceInDB, ["Catherine Go",False])
+        //push(referenceInDB, ["Zach AbuRahma",False])
     }
 })
 
